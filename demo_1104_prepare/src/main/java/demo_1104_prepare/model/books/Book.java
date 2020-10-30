f3f82cd1c3e1fa62c5c0cdbd95ac03f0fa47c05e
D043888 <D043888@192.168.0.101>
2020-10-30 10:12:52 +0100

    equals/hashCode

--- demo_1104_prepare/src/main/java/demo_1104_prepare/model/books/Book.java
+++ demo_1104_prepare/src/main/java/demo_1104_prepare/model/books/Book.java
@@ -32,5 +32,42 @@ public class Book
 	{
 		return title;
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null)
+		{
+			if (other.author != null)
+				return false;
+		}
+		else if (!author.equals(other.author))
+			return false;
+		if (title == null)
+		{
+			if (other.title != null)
+				return false;
+		}
+		else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 	
 }
